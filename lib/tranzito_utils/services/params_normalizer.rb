fb5903aa1e94e74a59017dfcb4c0a31d48955ee0
sethherr <seth.william.herr@gmail.com>
2022-09-26 09:06:59 -0700

    Add params normalizer to tranzito_utils

---
+++ lib/tranzito_utils/services/params_normalizer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ParamsNormalizer
+  def self.boolean(param = nil)
+    return false if param.blank?
+    ActiveRecord::Type::Boolean.new.cast(param.to_s.strip)
+  end
+end
