7df029aa0c24452eb8d688eab41119fcba36a1d3
Hafiz Ahmed <hafiz@tranzito.org>
2022-11-09 07:39:15 -0600

    Added pagination partail and JS

---
+++ tranzito_utils_js/dist/pagination.js
@@ -0,0 +1,16 @@
+export class Pagination {
+  initPerPageSelect () {
+    document.querySelector('#per_page_select').addEventListener('change', function (e) {
+      const urlParams = new URLSearchParams(window.location.search)
+      urlParams.delete('per_page')
+      urlParams.append('per_page', document.querySelector('#per_page_select').value)
+      window.location = `${location.pathname}?${urlParams.toString()}`
+    })
+  }
+
+  init () {
+    this.initPerPageSelect()
+  }
+}
+
+export default Pagination
--- tranzito_utils_js/index.js
+++ tranzito_utils_js/index.js
@@ -1,5 +1,6 @@
 // TimeParser is the npm packages, which require moment as dependency to handle the time parsing logic.
 import TimeParser from './dist/time_parser'
 import PeriodSelector from './dist/period_selector'
+import Pagination from './dist/pagination'
 
-export { TimeParser, PeriodSelector }
+export { TimeParser, PeriodSelector, Pagination }
