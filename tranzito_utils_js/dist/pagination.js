6e7a3188e3a7d1aa978f8eb844f2d6d63cf454ee
Hafiz Ahmed <hafiz@tranzito.org>
2022-11-17 15:01:18 -0600

    Fixed pagination issue

--- tranzito_utils_js/dist/pagination.js
+++ tranzito_utils_js/dist/pagination.js
@@ -1,6 +1,6 @@
 export class Pagination {
   initPerPageSelect () {
-    document.querySelector('#per_page_select').addEventListener('change', function (e) {
+    document.querySelector('#per_page_select')?.addEventListener('change', function (e) {
       const urlParams = new URLSearchParams(window.location.search)
       urlParams.delete('per_page')
       urlParams.append('per_page', document.querySelector('#per_page_select').value)
