776d410cfede958adf563c55aae51788290a6f53
Hafiz Ahmed <hafiz@tranzito.org>
2023-03-01 16:58:53 -0600

    code refactor

--- tranzito_utils_js/dist/dropdown_period_selector.js
+++ tranzito_utils_js/dist/dropdown_period_selector.js
@@ -4,53 +4,41 @@ import { urlForPeriod } from '../utils/period_select'
 class DropdownPeriodSelector {
   init () {
     this.initDropdownPeriodSelector()
-    this.initCustomPeriodSelector()
   }
 
   initDropdownPeriodSelector () {
     document.getElementById('periodSelectDropdown')
       .addEventListener('click', () => {
-        const selectedPeriod = document.querySelector('#periodSelectDropdown .dropdown-item.active')
-          .children[0].attributes['data-period'].value
-        if (selectedPeriod === 'custom') {
-          const startDate = document.querySelector('#dropdownCustomSelectionForm #start_time_selector').value
-          const endDate = document.querySelector('#dropdownCustomSelectionForm #end_time_selector').value
-          this.initDatePicker(startDate, endDate)
-        }
+        const selectedPeriod = document.querySelector('#periodSelectDropdown .dropdown-item a.active')
+          .attributes['data-period'].value
+        if (selectedPeriod === 'custom') this.initCustomPeriodSelector()
       })
     this.enablePeriodSelection()
   }
 
   enablePeriodSelection () {
-    document.querySelectorAll('#periodSelectDropdown .dropdown-item').forEach((dropdwonLink) => {
+    document.querySelectorAll('#periodSelectDropdown .dropdown-item a').forEach((dropdwonLink) => {
       dropdwonLink.addEventListener('click', (e) => {
-        document.querySelector('#periodSelectDropdown .dropdown-item.active').classList.remove('active')
+        if(dropdwonLink.attributes['data-period'].value == 'custom') e.preventDefault()
+
+        document.querySelector('#periodSelectDropdown .dropdown-item a.active').classList.remove('active')
         e.currentTarget.classList.add('active')
       })
     })
   }
 
-  initDatePicker (startDate, endDate) {
-    startDate = startDate || new Date()
-    endDate = endDate || new Date()
+  initCustomPeriodSelector () {
+    const startDate = document.querySelector('#dropdownCustomSelectionForm #start_time_selector').value
+    const endDate = document.querySelector('#dropdownCustomSelectionForm #end_time_selector').value
 
     flatpickr('#customPeriodSelectDatePicker', {
       mode: 'range',
       defaultDate: [startDate, endDate],
-      arrowTop: false,
-      prevArrow: '<<',
-      nextArrow: '>>',
       dateFormat: 'Y-m-d',
       disable: [(date) => date >= new Date()],
-      onValueUpdate: (selectedDates, dateStr, instance) => this.updateCustomDatePicker(selectedDates)
+      onValueUpdate: (selectedDates, dateStr, instance) => this.updateCustomDatePicker(selectedDates),
     }).open()
-  }
-
-  initCustomPeriodSelector () {
-    document.querySelector('#periodSelectDropdown .dropdown-item #showDateField')
-      .addEventListener('click', () => {
-        this.initDatePicker()
-      })
+    document.getElementsByClassName('flatpickr-calendar')[0].classList.remove('arrowTop')
   }
 
   updateCustomDatePicker (datesArr) {
